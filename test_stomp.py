f832e3d66062681b4ae57146ce94a607c0e5f1c9
james <git@mus-ic.co.uk>
2026-01-08 20:28:20 +0000

    adding the Flask business

--- test_stomp.py
+++ test_stomp.py
@@ -6,9 +6,11 @@ from time import sleep
 import os
 from dotenv import load_dotenv
 import requests
+import threading
 
 # Third party
 import stomp
+from flask import Flask
 
 # Internal
 from utils import td, trust
@@ -18,8 +20,21 @@ feed_username = os.getenv("FEED_USERNAME")
 feed_password = os.getenv("FEED_PASSWORD")
 hostname = os.getenv("HOSTNAME")
 port = os.getenv("PORT")
-locs_from = [loc.split(":")[0] for loc in os.getenv("LOCS").split(",")]
-locs_to = [loc.split(":")[1] for loc in os.getenv("LOCS").split(",")]
+locs_from = [
+    (loc.split(":")[0], loc.split(":")[2]) for loc in os.getenv("LOCS").split(",")
+]
+locs_to = [
+    (loc.split(":")[1], loc.split(":")[2]) for loc in os.getenv("LOCS").split(",")
+]
+tiploc_code = os.getenv("TIPLOC_CODE")
+headcodes = [headcode for headcode in os.getenv("HEADCODES", "").split(",")]
+
+# Create dictionaries for faster lookup
+locs_from_dict = {loc: dir for loc, dir in locs_from}
+locs_to_dict = {loc: dir for loc, dir in locs_to}
+
+# Global dictionary to store current state of approaching trains
+approaching_trains = {}  # headcode: {'direction': dir, 'location': td_from, 'timestamp': ...}
 
 
 class Listener(stomp.ConnectionListener):
@@ -38,7 +53,7 @@ class Listener(stomp.ConnectionListener):
             print(f"Found {len(services)} services for {headcode}")
             for service in services:
                 for location in service.get("schedule_location"):
-                    if location.get("tiploc_code") == "BSTEDMS":
+                    if location.get("tiploc_code") == tiploc_code:
                         service_details = {
                             "train_uid": service.get("CIF_train_uid"),
                             "atoc": service.get("atoc_code_description"),
@@ -59,35 +74,35 @@ class Listener(stomp.ConnectionListener):
             # Acknowledging messages is important in client-individual mode
             self._mq.ack(id=headers["message-id"], subscription=headers["subscription"])
 
-        # not a whole lot of point in using this
-        # if "TRAIN_MVT_" in headers["destination"]:
-        #     if trust_data := trust.parse_trust_frame(parsed_body):
-        #         if trust_data["headcode"] in ("4M76", "2W29", "2E80"):
-        #             print(trust_data["summary"])
-        #             print(json.dumps(trust_data["body"], indent=2))
-
         # elif "TD_" in headers["destination"]:
         if "TD_" in headers["destination"]:
             if td_data := td.parse_td_frame(parsed_body):
                 for td_entry in td_data:
+                    headcode = td_entry.get("description")
                     td_from = f"{td_entry.get('area_id')}{td_entry.get('from')}"
                     td_to = f"{td_entry.get('area_id')}{td_entry.get('to')}"
-                    if td_from in locs_from or td_to in locs_to:
-                        headcode = td_entry.get("description")
-                        print(f"Headcode: [{headcode}]")
+                    timestamp = td_entry.get("timestamp")
+
+                    # If the td_from is in locs_from, the train is coming
+                    if td_to in locs_from_dict:
+                        direction = locs_from_dict[td_to]
+                        print(f"Train coming from {td_from}, direction: {direction}")
                         if len(headcode) == 4:
                             service = self.get_service(headcode)
-                            print(json.dumps(service, indent=2))
-                        print(
-                            "{} [{:2}] {:2} {:4} {:>5}->{:5}".format(
-                                td_entry.get("timestamp"),
-                                td_entry.get("type"),
-                                td_entry.get("area_id"),
-                                td_entry.get("description"),
-                                td_entry.get("from"),
-                                td_entry.get("to"),
-                            )
-                        )
+                            approaching_trains[headcode] = {
+                                "direction": direction,
+                                "location": td_from,
+                                "headcode": headcode,
+                                "timestamp": timestamp,
+                                **service,
+                            }
+                            print(json.dumps(approaching_trains[headcode], indent=2))
+                    # If the td_from is in locs_to, the train has left
+                    elif td_from in locs_to_dict:
+                        if headcode in approaching_trains:
+                            del approaching_trains[headcode]
+                        direction = locs_to_dict[td_from]
+                        print(f"Train has left {td_from}, direction: {direction}")
 
         # else:
         #     print("Unknown destination: ", headers["destination"])
@@ -100,74 +115,74 @@ class Listener(stomp.ConnectionListener):
 
 
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument(
-    #     "-d",
-    #     "--durable",
-    #     action="store_true",
-    #     help="Request a durable subscription. Note README before trying this.",
-    # )
-    # action = parser.add_mutually_exclusive_group(required=False)
-    # action.add_argument(
-    #     "--td", action="store_true", help="Show messages from TD feed", default=True
-    # )
-    # action.add_argument(
-    #     "--trust", action="store_true", help="Show messages from TRUST feed"
-    # )
+    # Flask app with static folder
+    app = Flask(__name__, static_folder="static", static_url_path="")
 
-    # args = parser.parse_args()
+    @app.route("/")
+    def index():
+        return app.send_static_file("index.html")
 
-    # https://stomp.github.io/stomp-specification-1.2.html#Heart-beating
-    # We're committing to sending and accepting heartbeats every 5000ms
+    @app.route("/trains")
+    def get_trains():
+        return approaching_trains
 
-    # check to see if the json is there
+    @app.route("/favicon.ico")
+    def favicon():
+        return app.send_static_file("favicon.ico")
 
-    td_connection = stomp.Connection(
-        [(hostname, port)],
-        keepalive=True,
-        heartbeats=(5000, 5000),
-    )
-    trust_connection = stomp.Connection(
-        [(hostname, port)],
-        keepalive=True,
-        heartbeats=(5000, 5000),
-    )
-    td_connection.set_listener("", Listener(td_connection, durable=True))
-    trust_connection.set_listener("", Listener(trust_connection))
-    # Connect to feed
-    td_connect_headers = {
-        "username": feed_username,
-        "passcode": feed_password,
-        "wait": True,
-        "client-id": feed_username,
-    }
-    trust_connect_headers = {
-        "username": feed_username,
-        "passcode": feed_password,
-        "wait": True,
-    }
+    # Function to run the STOMP listener in a thread
+    def run_listener():
+        td_connection = stomp.Connection(
+            [(hostname, port)],
+            keepalive=True,
+            heartbeats=(5000, 5000),
+        )
+        trust_connection = stomp.Connection(
+            [(hostname, port)],
+            keepalive=True,
+            heartbeats=(5000, 5000),
+        )
+        td_connection.set_listener("", Listener(td_connection, durable=True))
+        trust_connection.set_listener("", Listener(trust_connection))
+        # Connect to feed
+        td_connect_headers = {
+            "username": feed_username,
+            "passcode": feed_password,
+            "wait": True,
+            "client-id": feed_username,
+        }
+        trust_connect_headers = {
+            "username": feed_username,
+            "passcode": feed_password,
+            "wait": True,
+        }
 
-    td_connection.connect(**td_connect_headers)
-    trust_connection.connect(**trust_connect_headers)
-    # Determine topic to subscribe
-    # topic = None
-    # if args.trust:
-    #     topic = "/topic/TRAIN_MVT_ALL_TOC"
-    # elif args.td:
-    td_topic = "/topic/TD_ANG_SIG_AREA"
-    trust_topic = "/topic/TRAIN_MVT_ALL_TOC"
+        td_connection.connect(**td_connect_headers)
+        trust_connection.connect(**trust_connect_headers)
+        td_topic = "/topic/TD_ANG_SIG_AREA"
+        trust_topic = "/topic/TRAIN_MVT_ALL_TOC"
 
-    # Subscription
-    td_subscribe_headers = {
-        "destination": td_topic,
-        "id": 1,
-        "activemq.subscriptionName": feed_username + td_topic,
-        "ack": "client-individual",
-    }
-    trust_subscribe_headers = {"destination": trust_topic, "id": 2}
-    td_connection.subscribe(**td_subscribe_headers)
-    print("Subscribed to TD topic")
-    trust_connection.subscribe(**trust_subscribe_headers)
-    print("Subscribed to TRUST topic")
-    while td_connection.is_connected() or trust_connection.is_connected():
-        sleep(1)
+        # Subscription
+        td_subscribe_headers = {
+            "destination": td_topic,
+            "id": 1,
+            "activemq.subscriptionName": feed_username + td_topic,
+            "ack": "client-individual",
+        }
+        trust_subscribe_headers = {"destination": trust_topic, "id": 2}
+        td_connection.subscribe(**td_subscribe_headers)
+        print("Subscribed to TD topic")
+        trust_connection.subscribe(**trust_subscribe_headers)
+        print("Subscribed to TRUST topic")
+        while td_connection.is_connected() or trust_connection.is_connected():
+            sleep(1)
+
+    # Start the listener in a separate daemon thread
+    listener_thread = threading.Thread(target=run_listener, daemon=True)
+    listener_thread.start()
+
+    # Run the Flask app (can be interrupted with CTRL-C)
+    try:
+        app.run(host="0.0.0.0", port=5000)
+    except KeyboardInterrupt:
+        print("\nShutting down gracefully...")
